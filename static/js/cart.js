711aa807fbf499d2d7b39f095339cf2639c2257c
Tarun Suryavanshi <68415757+tarun163@users.noreply.github.com>
2026-08-21 18:16:38 +0530

    Update for loop to start from 1

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,37 +1,29 @@
-var updateBtns = document.getElementsByClassName('update-cart')
-for (var i = 0; i < updateBtns.length; i++) {
+var updateBtns = document.getElementsByClassName('update-cart');
+for (var i = 1; i < updateBtns.length; i++) {
     updateBtns[i].addEventListener('click', function() {
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('productId:', productId, 'Action:', action)
-
-        console.log(user)
-        if (user === 'AnonymousUser') {
-            console.log('not logged in')
-        } else {
-            updateUserOrder(productId, action)
-        }
-    })
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
+        console.log('productId:', productId, 'Action:', action);
+        updateUserOrder(productId, action);
+    });
 }
 
 function updateUserOrder(productId, action) {
-    console.log('user is logged in')
-    var url = '/update_Item/'
-
+    console.log('user is logged in');
+    var url = '/update_item/';
     fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': csrftoken,
-            },
-            body: JSON.stringify({ 'productId': productId, 'action': action })
-        })
-        .then((response) => {
-            return response.json()
-        })
-
-    .then((data) => {
-        console.log('data:', data)
-        location.reload()
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({ 'productId': productId, 'action': action })
     })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log('data:', data);
+        location.reload();
+    });
 }
